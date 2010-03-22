c8f76b05ae31c0eda80dfa4c29a661823c7570d5
Andre Engelbrecht <andre@teh-node.co.za>
2010-03-22 17:03:30 +0000

    Added admin actions for blog entries

--- src/tehblog/admin.py
+++ src/tehblog/admin.py
@@ -9,6 +9,32 @@ from django.contrib import admin
 
 from tehblog.models import Entry, Category
 
+### Admin Actions
+def publish(modeladmin, request, queryset):
+    queryset.update(status=Entry.PUBLIC)
+    # call every item's save() method so that dates are
+    # also updated
+    for item in queryset:
+        item.save()
+publish.short_description = "Publish selected Entries"
+
+def retract(modeladmin, request, queryset):
+    queryset.update(status=Entry.DRAFT)
+retract.short_description = "Retract selected Entries"
+
+def mark_for_review(modeladmin, request, queryset):
+    queryset.update(status=Entry.REVIEW)
+mark_for_review.short_description = "Review selected Entries"
+
+def allow_comments(modeladmin, request, queryset):
+    queryset.update(allow_comments=True)
+allow_comments.short_description = "Comments - Allow"
+
+def disallow_comments(modeladmin, request, queryset):
+    queryset.update(allow_comments=False)
+disallow_comments.short_description = "Comments - Disallow"
+
+### Admin Classes
 class EntryAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ['title']}
     list_display = ['title', 'slug', 'status', 'author', 'created_date',
@@ -16,6 +42,8 @@ class EntryAdmin(admin.ModelAdmin):
     list_filter = ['status', 'author', 'publish_date', 'allow_comments']
     search_fields = ['title', 'slug', 'extract', 'content']
     date_hierarchy = 'publish_date'
+    actions = [publish, retract, mark_for_review, allow_comments,
+               disallow_comments]
 
 class CategoryAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ['title']}
