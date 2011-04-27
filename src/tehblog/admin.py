47f7efec5426b10e71cdc9a5671f3c6d083bb0a0
Andre Engelbrecht <andre@teh-node.co.za>
2011-04-27 09:45:17 +0100

    Forgot to update the admin to make use of the new StateMachine

--- src/tehblog/admin.py
+++ src/tehblog/admin.py
@@ -8,22 +8,39 @@
 from django.contrib import admin
 
 from tehblog.models import Entry, Category
+from ostinato.admin import statemachine_form
+from ostinato.statemachine import InvalidAction
 
 ### Admin Actions
 def publish(modeladmin, request, queryset):
-    queryset.update(status=Entry.PUBLIC)
-    # call every item's save() method so that dates are
-    # also updated
+    # TODO: We need a more efficient method to do this, since it's not 'lazy'
     for item in queryset:
-        item.save()
+        try:
+            item.sm_take_action('Publish')
+            item.save()
+        except InvalidAction:
+            pass
 publish.short_description = "Publish selected Entries"
 
 def retract(modeladmin, request, queryset):
-    queryset.update(status=Entry.DRAFT)
+    # TODO: We need a more efficient method to do this, since it's not 'lazy'
+    for item in queryset:
+        if 'Reject' in item.sm_state_actions():
+            item.sm_take_action('Reject')
+            item.save()
+        elif 'Retract' in item.sm_state_actions():
+            item.sm_take_action('Retract')
+            item.save()
 retract.short_description = "Retract selected Entries"
 
 def mark_for_review(modeladmin, request, queryset):
-    queryset.update(status=Entry.REVIEW)
+    # TODO: We need a more efficient method to do this, since it's not 'lazy'
+    for item in queryset:
+        try:
+            item.sm_take_action('Submit')
+            item.save()
+        except InvalidAction:
+            pass
 mark_for_review.short_description = "Review selected Entries"
 
 def allow_comments(modeladmin, request, queryset):
@@ -36,11 +53,14 @@ disallow_comments.short_description = "Comments - Disallow"
 
 ### Admin Classes
 class EntryAdmin(admin.ModelAdmin):
+    form = statemachine_form(Entry)
+
     prepopulated_fields = {'slug': ['title']}
-    list_display = ['title', 'slug', 'status', 'author', 'created_date',
-                    'modified_date', 'publish_date', 'allow_comments']
-    list_filter = ['status', 'author', 'publish_date', 'allow_comments']
-    search_fields = ['title', 'slug', 'extract', 'content']
+    list_display = ['title', 'slug', 'sm_state_admin', 'author',
+                    'created_date', 'modified_date', 'publish_date',
+                    'allow_comments']
+    list_filter = ['_sm_state', 'author', 'publish_date', 'allow_comments']
+    search_fields = ['title', 'slug', 'extract', 'content', '_sm_state']
     date_hierarchy = 'publish_date'
     actions = [publish, retract, mark_for_review, allow_comments,
                disallow_comments]
