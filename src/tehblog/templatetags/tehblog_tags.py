444784de839381e46e490d7569ecf0c41ab055ee
Andre Engelbrecht <andre@teh-node.co.za>
2011-05-06 20:34:38 +0100

    Added a special EmbedFilter class that will allow us to write custom functions to easilly embed content within blog entries. This is specifically aimed at content that will be using markup filters like Markdown.

--- src/tehblog/templatetags/tehblog_tags.py
+++ src/tehblog/templatetags/tehblog_tags.py
@@ -5,15 +5,13 @@
 # Please see the text file LICENCE for more information
 # If this script is distributed, it must be accompanied by the Licence
 
+import re
+
 from django import template
 from django.db.models import Count
 
+from tagging.models import Tag, TaggedItem
 from tehblog.models import Entry, Category 
-
-try:
-    from tagging.models import Tag, TaggedItem
-except ImportError:
-    pass
 
 register = template.Library()
 
@@ -37,8 +35,8 @@ def tag_list(slice_count=None):
     Requires django-tagging.
     Renders a list of Tags used for all published blog entries.
 
-    ``slice_count`` is the number of items that the list in the template should
-    be sliced to
+    ``slice_count`` is the number of items that the list in the
+    template should be sliced to
 
     """
     slice_count = str(slice_count)
@@ -121,3 +119,77 @@ def entries_for_month(date_value):
         publish_date__month=date_value.month,
     ).count()
     return count
+
+class EmbedFilter(object):
+    """
+    Special class to register render functions for use with the embed
+    filter.
+    """
+    _functions = []
+
+    @classmethod
+    def register(cls, func_name, func):
+        cls._functions.append({'name': func_name, 'function': func})
+
+    @classmethod
+    def get_function(cls, func_name):
+        if cls._functions:
+            for f in cls._functions:
+                if f['name'] == func_name: return f['function']
+        return None
+
+    @classmethod
+    def get_functions(cls):
+        to_return = []
+        for func in cls._functions:
+            to_return.append(func['function'])
+        return to_return
+    
+@register.filter(name='embed')
+def embed(content, function=None):
+    """
+    This filter will call func() with content being passed to it as a
+    string.
+
+    ``function`` - A string containing the name of the function that
+    parses content. The funciton will typically perform a task like,
+    looking for a specific regex pattern in ``content`` and replace
+    that pattern with Html that renders the element.
+
+    If ``function`` is not supplied, we will automatically apply all
+    functions to the filter.
+    """
+    if function:
+        func = EmbedFilter.get_function(function)
+        if func: return func(content)
+        else: return content
+    else:
+        for func in EmbedFilter.get_functions():
+            content = func(content)
+        return content
+
+def youtube(content):
+    """
+    Looks for any youtube url patterns in content, and replaces it with
+    the youtube video
+    """
+    regex = re.compile(r"(http://)?(www\.)?((youtu\.be/)|(youtube\.com/watch\?v=))(?P<id>[A-Za-z0-9\-=_]{11})")
+    return regex.sub('''
+        <iframe width="480" height="390"
+            src="http://www.youtube.com/embed/\g<id>" frameborder="0"
+            allowfullscreen></iframe>
+    ''', content)
+
+def pl_gallery(content):
+    """
+    Example function showing how a photologue gallery can be
+    embedded based on it's slug.
+
+    Note: This is just an example and is a pretty pointless function to
+    use as is. But free to copy this and modify the result.
+
+    """
+    regex = re.compile(r"\{{3}gallery=(?P<slug>[-\w]+)\}{3}")
+    return regex.sub('''PHOTOLOGUE GALLERY \g<slug> HERE''', content)
+
+EmbedFilter.register('youtube', youtube)
